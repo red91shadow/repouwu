4c5c69486bb5ca1047ec415b417044af084618a5
Emil2245 <emilmontiel@hotmail.com>
2023-06-04 02:54:43 -0500

    pruebas: ya c pudo el cuadrado

--- app/src/main/java/ec/edu/uce/pa/geometrias/Linea.java
+++ app/src/main/java/ec/edu/uce/pa/geometrias/Linea.java
@@ -6,73 +6,81 @@ import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
+
 public class Linea {
     private FloatBuffer bufferVertices;
     private FloatBuffer bufferColores;
-    private final static int byteFlotante =4; //son 4 por que en  un flotante hay 4 bytes
-    private final static int compPorVertice=2;
-    private final static int compPorColores=4;
+    private final static int byteFlotante = 4; // 4 bytes per float
+    private final static int compPorVertice = 2;
+    private final static int compPorColores = 4;
+
     public Linea() {
-        float[] verices = {
-                4.0f,4.0f,   //0
-                4.0f,-4.0f,  //1
-                -4.0f,-4.0f,  //2
-                -4.0f,4.0f   //3
+        float[] vertices = {
+                4.0f, 4.0f,   // 0
+                4.0f, -4.0f,  // 1
+                -4.0f, -4.0f, // 2
+                -4.0f, 4.0f,  // 3
+                4.0f, 4.0f    // 0
         };
         float[] colores = {
-                1.0f, 0.0f, 0.0f, 1.0f,//rojo se le asigna al vertice 0
-                0.0f, 1.0f, 0.0f, 1.0f,//verde se le asigna al vertice 1
-                0.0f, 0.0f, 1.0f, 1.0f,//azul se le asigna al vertice 2
-                1.0f, 1.0f, 0.0f, 1.0f,//amarillo se le asigna al vertice 3
+                1.0f, 0.0f, 0.0f, 1.0f, // red assigned to vertex 0
+                0.0f, 1.0f, 0.0f, 1.0f, // green assigned to vertex 1
+                0.0f, 0.0f, 1.0f, 1.0f, // blue assigned to vertex 2
+                1.0f, 0.0f, 1.0f, 1.0f, // yellow assigned to vertex 3
+                1.0f, 0.0f, 1.0f, 1.0f  // yellow assigned to vertex 3
         };
 
-
-        ByteBuffer buffer = ByteBuffer.allocateDirect(verices.length*byteFlotante);
+        ByteBuffer buffer = ByteBuffer.allocateDirect(vertices.length * byteFlotante);
         buffer.order(ByteOrder.nativeOrder());
         bufferVertices = buffer.asFloatBuffer();
-        bufferVertices.put(verices);
+        bufferVertices.put(vertices);
         bufferVertices.position(0);
 
-        buffer = ByteBuffer.allocateDirect(colores.length*byteFlotante);
+        buffer = ByteBuffer.allocateDirect(colores.length * byteFlotante);
         buffer.order(ByteOrder.nativeOrder());
         bufferColores = buffer.asFloatBuffer();
         bufferColores.put(colores);
         bufferColores.position(0);
     }
 
-    public void dibujar (GL10 gl){
-        gl.glFrontFace(gl.GL_CW);
+    public void dibujar(GL10 gl) {
+        gl.glFrontFace(GL10.GL_CW);
         bufferVertices.position(0);
-        gl.glVertexPointer(compPorVertice, gl.GL_FLOAT, 0, bufferVertices);
-        gl.glEnableClientState(gl.GL_VERTEX_ARRAY);
+        gl.glVertexPointer(compPorVertice, GL10.GL_FLOAT, 0, bufferVertices);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 
         bufferColores.position(0);
-        gl.glColorPointer(compPorColores, gl.GL_FLOAT,0,bufferColores);
-        gl.glEnableClientState(gl.GL_COLOR_ARRAY);
+        gl.glColorPointer(compPorColores, GL10.GL_FLOAT, 0, bufferColores);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
 
-        gl.glLineWidth(10);//para el tamaño del punto
-        gl.glDrawArrays(gl.GL_LINES,0,4);
+        // Set the line width
+        gl.glLineWidth(2.0f);
 
-        /*// este es una forma pero no es la màs optioma para eso va la matriz de colores
-        //le asigno un color a el vertice que corresponde al indice 0
-        gl.glColor4f(1.0f, 0.0f,0.0f,1.0f);
-        gl.glDrawArrays(gl.GL_POINTS,0,1);
+        // Calculate the step size for the dashed line segments
+        float dashLength = 0.1f;
+        float gapLength = 0.1f;
+        float segmentLength = dashLength + gapLength;
 
-        //le asigno un color a el vertice que corresponde al indice 1
-        gl.glColor4f(0.0f, 1.0f,0.0f,1.0f);
-        gl.glDrawArrays(gl.GL_POINTS,1,1);
+        // Draw the dashed line segments
+        for (int i = 0; i < 4; i++) {
+            float x1 = bufferVertices.get(i * 2);
+            float y1 = bufferVertices.get(i * 2 + 1);
+            float x2 = bufferVertices.get((i + 1) * 2);
+            float y2 = bufferVertices.get((i + 1) * 2 + 1);
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+            float length = (float) Math.sqrt(dx * dx + dy * dy);
+            float segmentCount = length / segmentLength;
 
-        //le asigno un color a el vertice que corresponde al indice 2
-        gl.glColor4f(0.0f, 0.0f,1.0f,1.0f);
-        gl.glDrawArrays(gl.GL_POINTS,2,1);
+            for (int j = 0; j < segmentCount; j++) {
+                if (j % 2 == 0) {
+                    gl.glDrawArrays(GL10.GL_LINES, (i * 2 + j) * 2, 2);
+                }
+            }
+        }
 
-        //le asigno un color a el vertice que corresponde al indice 2
-        gl.glColor4f(1.0f, 0.5f,3.0f,1.0f);
-        gl.glDrawArrays(gl.GL_POINTS,3,1);
-        */
-
-        gl.glFrontFace(gl.GL_CCW);
-        gl.glDisableClientState(gl.GL_VERTEX_ARRAY);
-        gl.glDisableClientState(gl.GL_COLOR_ARRAY);
+        gl.glFrontFace(GL10.GL_CCW);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
     }
 }
--- app/src/main/java/ec/edu/uce/pa/renders/RenderLinea.java
+++ app/src/main/java/ec/edu/uce/pa/renders/RenderLinea.java
@@ -13,7 +13,7 @@ public class RenderLinea implements GLSurfaceView.Renderer {
     private Linea linea ;
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig eglConfig) {
-        gl.glClearColor(0.5f, 0.5f,0.5f,1.0f);
+        gl.glClearColor(0.8f, 0.8f,0.8f,1.0f);
         linea = new Linea();
     }
 
@@ -21,7 +21,7 @@ public class RenderLinea implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 gl, int ancho, int alto) {
         gl.glViewport(0,0,ancho, alto);//origen "x=0" y "y=0" por defecto alto y ancho de la pantalla, es practicamente la ventana de copordenas donde se va a dibujar
         gl.glMatrixMode(gl.GL_PROJECTION);
-        gl.glFrustumf(-5, 5, -5,5,3,30);// es la mas usada
+        gl.glFrustumf(-4, 4, -4,4,3,30);// es la mas usada
         //gl.glOrthof(-5,5,-5,5,1,30);//en projecciòn ortogonal se usa depende a lo que se estè programando
     }
 
@@ -31,11 +31,11 @@ public class RenderLinea implements GLSurfaceView.Renderer {
         gl.glMatrixMode(gl.GL_MODELVIEW); //matriz modelo
         gl.glLoadIdentity();//centro de cordenadas
         gl.glTranslatef(0.0f, 0.0f,-4.0f); //la posiciòn donde se dibujarà
-        //gl.glScalef(1,2,1);//1 no escalo, 2 escalo el doble, en este caso se escala en el eje y
-        vIncremento += 0.1;
-        gl.glRotatef(vIncremento, 0,0,1);//rota el valor de incremento  grados en el eje z
+//        gl.glScalef(1,2,1);//1 no escalo, 2 escalo el doble, en este caso se escala en el eje y
+//        vIncremento += 0.1;
+//        gl.glRotatef(vIncremento, 0,0,1);//rota el valor de incremento  grados en el eje z
         linea.dibujar(gl);//dibuja una vez el punto
-        gl.glTranslatef(0.0f, 0.0f,-2f);//se traslada en el eje z
+        gl.glTranslatef(0.0f, 0.0f,-1.0f);//se traslada en el eje z
         linea.dibujar(gl); //dibuja una segunda vez el punto
 
 
