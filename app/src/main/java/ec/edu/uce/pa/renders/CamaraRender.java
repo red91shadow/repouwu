c2689988a146f85ea0867b3c12e8d3b6040126e2
Emil2245 <emilmontiel@hotmail.com>
2023-06-17 00:44:57 -0500

    proyecto completo

--- app/src/main/java/ec/edu/uce/pa/renders/CamaraRender.java
+++ app/src/main/java/ec/edu/uce/pa/renders/CamaraRender.java
@@ -7,7 +7,6 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import ec.edu.uce.pa.geometrias.CuboColores;
-
 public class CamaraRender implements GLSurfaceView.Renderer {
     private float vIncremento;
     private float i = -5;
@@ -25,7 +24,7 @@ public class CamaraRender implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl, int ancho, int alto) {
         float aspectRatio = ((float) alto / (float) ancho);
-        gl.glViewport(0, 0, ancho, alto);//origen "x=0" y "y=0" por defecto alto y ancho de la pantalla, es practicamente la ventana de copordenas donde se va a dibujar
+        gl.glViewport(0, 0, ancho, alto);
         gl.glMatrixMode(gl.GL_PROJECTION);
         gl.glLoadIdentity();
         gl.glFrustumf(-aspectRatio, aspectRatio, -3, 3, 2f, 15);// es la mas usada
@@ -45,12 +44,13 @@ public class CamaraRender implements GLSurfaceView.Renderer {
                 0,0,0,
                 0,0,1);
         cubo.dibujar(gl);
+
         gl.glTranslatef(0, 2, 0);
-        gl.glScalef(0.3f,0.3f,0.3f);
+        gl.glScalef(0.5f,0.5f,0.5f);
         cubo.dibujar(gl);
 
         vIncremento += 0.5f;
-        i += 0.1f;
+        i += 0.056f;
     }
 
 }
