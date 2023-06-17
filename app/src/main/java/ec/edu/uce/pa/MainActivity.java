9ec77ae5692c8ed615c7dba1cd67eb63f99532e2
Emil2245 <emilmontiel@hotmail.com>
2023-06-17 10:35:41 -0500

    deber rotar cubito

--- app/src/main/java/ec/edu/uce/pa/MainActivity.java
+++ app/src/main/java/ec/edu/uce/pa/MainActivity.java
@@ -1,14 +1,26 @@
 package ec.edu.uce.pa;
 
 
-import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
-import ec.edu.uce.pa.renders.CamaraRender;
+import androidx.appcompat.app.AppCompatActivity;
+
 import ec.edu.uce.pa.renders.RenderCuboColores;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    public static float rotarX =0;
+    public static float rotarY =0;
+    public static float rotarZ =0;
+    public static float ejeX =0;
+    public static float ejeY =0;
+    public static float ejeZ =-3;
+    public static float iRotacion=0,iRotacionC=0;
+    public static boolean rotar=false;
+    public static boolean ejeCamara = false;
 
     private GLSurfaceView glSurfaceView;
 
@@ -16,7 +28,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setRenderer(new CamaraRender());
+        //manualmente
+        glSurfaceView.setRenderer(new RenderCuboColores());
         setContentView(glSurfaceView);
     }
 
@@ -30,5 +43,73 @@ public class MainActivity extends Activity {
     protected void onPause() {
         super.onPause();
         glSurfaceView.onPause();
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_W:
+                ejeY +=0.1f;
+                return true;
+            case KeyEvent.KEYCODE_S:
+                ejeY -=0.1f;
+                return true;
+            case KeyEvent.KEYCODE_A:
+                ejeX -=0.1f;
+                return true;
+            case KeyEvent.KEYCODE_D:
+                ejeX +=0.1f;
+                return true;
+            case KeyEvent.KEYCODE_Q:
+                ejeZ -=0.1f;
+                return true;
+            case KeyEvent.KEYCODE_E:
+                ejeZ +=0.1f;
+                return true;
+
+            case KeyEvent.KEYCODE_DPAD_UP:
+                ejeCamara = false;
+                rotar = true;
+                rotarX = 1;
+                rotarY = 0;
+                rotarZ = 0;
+                iRotacion-=2.5f;
+                iRotacionC-=0.01f;
+                return true;
+
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                ejeCamara = false;
+                rotar = true;
+                rotarX = 1;
+                rotarY = 0;
+                rotarZ = 0;
+                iRotacion+=2.5f;
+                iRotacionC+=0.01f;
+                return true;
+
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                ejeCamara = true;
+                rotar = true;
+                rotarX = 0;
+                rotarY = 1;
+                rotarZ = 0;
+                iRotacion-=2.5f;
+                iRotacionC+=0.01f;
+                return true;
+
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                ejeCamara = true;
+                rotar = true;
+                rotarX = 0;
+                rotarY = 1;
+                rotarZ = 0;
+                iRotacion+=2.5f;
+                iRotacionC-=0.01f;
+                return true;
+            default:
+                Toast.makeText(this,"Otra tecla" , Toast.LENGTH_SHORT).show();
+                return true;
+        }
     }
 }
