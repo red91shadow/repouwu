460c967e7ac86f968a76be7b3ed56fd356466160
Emil2245 <emilmontiel@hotmail.com>
2023-06-18 20:06:19 -0500

    deber rotar cubito

---
+++ app/src/main/java/ec/edu/uce/pa/renders/RenderCuboColores.java
@@ -0,0 +1,43 @@
+package ec.edu.uce.pa.renders;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import ec.edu.uce.pa.MainActivity;
+import ec.edu.uce.pa.geometrias.CuboColores;
+
+public class RenderCuboColores implements GLSurfaceView.Renderer {
+    private CuboColores prisma;
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig eglConfig) {
+        gl.glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        prisma = new CuboColores();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int ancho, int alto) {
+        float aspectRatio = ((float) alto / (float) ancho);
+        gl.glViewport(0, 0, ancho, alto);
+        gl.glMatrixMode(gl.GL_PROJECTION);
+        gl.glFrustumf(-aspectRatio, aspectRatio, -3.0f, 3.0f, 2.0f, 15.0f);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
+        gl.glMatrixMode(gl.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(MainActivity.ejeX, MainActivity.ejeY, MainActivity.ejeZ);
+        if(MainActivity.rotar){
+            gl.glRotatef(MainActivity.iRotacion, MainActivity.rotarX, MainActivity.rotarY, MainActivity.rotarZ);
+            prisma.dibujar(gl);
+            System.out.println("2");
+        }else{
+            prisma.dibujar(gl);
+        }
+    }
+}
